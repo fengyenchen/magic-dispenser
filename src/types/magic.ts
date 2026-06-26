db9c227208d9bd3af901c52b6e8cd8fc6f38cefc
fengyenchen <feng20050504@gmail.com>
2026-06-27 00:49:06 +0800

    feat(types): add magicItem, role, cartItem, and order interfaces

---
+++ src/types/magic.ts
@@ -0,0 +1,30 @@
+export interface Role {
+  id: string;
+  account: string;
+  password: string;
+  username: string;
+  role: 'student' | 'professor';
+}
+
+export interface MagicItem { // 商品屬性
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: 'ingredient' | 'spell'; // 魔藥原料 or 法術
+}
+
+export interface CartItem { // 購物車某項商品屬性
+  product: MagicItem;
+  quantity: number;
+}
+
+export interface Order { // 訂單屬性
+    id: string;
+    userId: string;
+    items: CartItem[];
+    totalPrice: number;
+    status: 'brewing' | 'completed' | 'failed';
+    createdAt: string;
+}
